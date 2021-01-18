5885da3aa88dfbca68e7dbff9e9b9f220d1eee76
Hydeo Watase <hydeowatase@gmail.com>
2021-01-17 23:58:48 -0300

    N3-Challenge-ReactJS - GoFinances Final.

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,14 +23,20 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO - need
+    if (!uploadedFiles.length) return;
+
+    const { file, name } = uploadedFiles[0];
+
+    data.append('file', file, name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
